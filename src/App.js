a8ae93b4b8fe316563daa4fceefec9ec594f189c
Jennifer Tchai <jentchai@gmail.com>
2022-05-05 23:32:08 -0400

    cleaned up App.js code

--- src/App.js
+++ src/App.js
@@ -4,32 +4,13 @@ import Pet from './Pet';
 
 const App = () => {
   return (
-    <div>
+    <div id="my-app">
       <h1>Adopt Me!</h1>
       <Pet name="Luna" animal="dog" breed="Golden Retriever" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
     </div>
-  )
-}
+  );
+};
 
-// const App = () => {
-//   return React.createElement("div", {}, [
-//     React.createElement("h1", {}, "Adopt Me!"),
-//     React.createElement(Pet, {
-//       name: "Luna",
-//       animal: "Dog",
-//       breed: "Golden Retriever",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Pepper",
-//       animal: "Bird",
-//       breed: "Cockatiel",
-//     }),
-//     React.createElement(Pet, {
-//       name: "Doink",
-//       animal: "Cat",
-//       breed: "Mix",
-//     }),
-//   ]);
-// };
-
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
