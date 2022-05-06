d3d8233e54c92ab173f0e7337de0d56ad72fb630
Jennifer Tchai <jentchai@gmail.com>
2022-05-05 23:52:46 -0400

    made SearchParams on frontpage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { render } from 'react-dom';
-import Pet from './Pet';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div id="my-app">
       <h1>Adopt Me!</h1>
-      <Pet name="Luna" animal="dog" breed="Golden Retriever" />
-      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-      <Pet name="Doink" animal="cat" breed="Mix" />
+      <SearchParams />
     </div>
   );
 };
