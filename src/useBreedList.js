3030ac62cb89ead913c93137895b56bf07fdb96a
Jennifer Tchai <jentchai@gmail.com>
2022-05-23 22:47:10 -0400

    added Details component

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,10 +1,11 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useDebugValue } from 'react';
 
 const localCache = {};
 
 export default function useBreedList(animal) {
     const [breedList, setBreedList] = useState([]);
     const [status, setStatus] = useState("unloaded");
+    useDebugValue("hi from the code");
 
     useEffect(() => {
         if (!animal) {
