7b739a5cf475d1f04a1e3a69bf8f93a5063156d8
Jennifer Tchai <jentchai@gmail.com>
2022-05-26 22:40:29 -0400

    starting to rewrite as class components

--- src/Details.js
+++ src/Details.js
@@ -9,6 +9,25 @@ import { useParams } from 'react-router-dom';
 class Details extends Component {
     constructor(props) {
         super(props);
+
+        this.state = { loading: true };
+    }
+
+    render() {
+        if (this.state.loading) {
+            return <h2>loading ...</h2>
+        }
+
+        const { animal, breed, city, state, description, name } = this.state;
+
+        return (
+            <div className="details">
+                <div>
+                    <h1>{name}</h1>
+                    <h2>{animal} - {breed} - {city}, {state}</h2>
+                </div>
+            </div>
+        );
     }
 }
 
