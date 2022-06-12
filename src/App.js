6e785427127dacae4257683c115e92e06a5f6979
Jennifer Tchai <jentchai@gmail.com>
2022-06-11 22:39:34 -0400

    fixed App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import { render } from 'react-dom';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import { StrictMode } from 'react';
-import SearchParams from './SearchParams';
-import Details from './Details';
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
+import { StrictMode } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 
 const App = () => {
   return (
     <StrictMode>
       <BrowserRouter>
         <header>
-         <Link to="/">Adopt Me!</Link>
+          <Link to="/">Adopt Me!</Link>
         </header>
         <Routes>
-          <Route path='/details/:id' element={<Details />} />
-          <Route path='/' element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<SearchParams />} />
         </Routes>
       </BrowserRouter>
     </StrictMode>
