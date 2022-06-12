ab490549edc6e400cb948127cd673e846e3d54de
Jennifer Tchai <jentchai@gmail.com>
2022-06-11 22:05:35 -0400

    render code

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,14 +2,18 @@ import { Component } from 'react';
 
 class Carousel extends Component {
   state = { 
-    active: 0
+    active: 0,
   }
 
 
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
-  
+
+  render () {
+    const { active } = this.state;
+    const { images } = this.props;
+  }
 }
 
 export default Carousel;
