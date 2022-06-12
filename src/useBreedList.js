e8b34ad7a2f3e65a9ffae2e10ba2472f0d3ff223
Jennifer Tchai <jentchai@gmail.com>
2022-06-11 22:42:49 -0400

    fixed useBreedList

--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,34 +1,32 @@
-import { useState, useEffect, useDebugValue } from 'react';
+import { useState, useEffect } from "react";
 
 const localCache = {};
 
 export default function useBreedList(animal) {
-    const [breedList, setBreedList] = useState([]);
-    const [status, setStatus] = useState("unloaded");
-    useDebugValue("hi from the code");
+  const [breedList, setBreedList] = useState([]);
+  const [status, setStatus] = useState("unloaded");
 
-    useEffect(() => {
-        if (!animal) {
-            setBreedList([]);
-        } else if (localCache[animal]) {
-            setBreedList(localCache[animal])
-        } else {
-            requestBreedList();
-        }
+  useEffect(() => {
+    if (!animal) {
+      setBreedList([]);
+    } else if (localCache[animal]) {
+      setBreedList(localCache[animal]);
+    } else {
+      requestBreedList();
+    }
 
-        async function requestBreedList() {
-            setBreedList([]);
-            setStatus("loading");
+    async function requestBreedList() {
+      setBreedList([]);
+      setStatus("loading");
+      const res = await fetch(
+        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+      );
+      const json = await res.json();
+      localCache[animal] = json.breeds || [];
+      setBreedList(localCache[animal]);
+      setStatus("loaded");
+    }
+  }, [animal]);
 
-            const res = await fetch (
-                `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-            )
-            const json = await res.json();
-            localCache[animal] = json.breeds || [];
-            setBreedList(localCache[animal]);
-            setStatus("loaded");
-        }
-    }, [animal]);
-
-    return [breedList, status];
+  return [breedList, status];
 }
