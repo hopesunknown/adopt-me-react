b6e584483663b072a4bfe5909d89af8360d0abe3
Jennifer Tchai <jentchai@gmail.com>
2022-06-11 22:28:52 -0400

    fixed Carousel

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,34 +1,33 @@
-import { Component } from 'react';
+import { Component } from "react";
 
 class Carousel extends Component {
-  state = { 
+  state = {
     active: 0,
-  }
-
+  };
 
   static defaultProps = {
     images: ["http://pets-images.dev-apis.com/pets/none.jpg"],
   };
 
-  render () {
+  render() {
     const { active } = this.state;
     const { images } = this.props;
-
     return (
       <div className="carousel">
         <img src={images[active]} alt="animal" />
         <div className="carousel-smaller">
           {images.map((photo, index) => (
+            // eslint-disable-next-line
             <img
               key={photo}
-              id={photo}
+              src={photo}
               className={index === active ? "active" : ""}
               alt="animal thumbnail"
             />
           ))}
         </div>
       </div>
-    )
+    );
   }
 }
 
