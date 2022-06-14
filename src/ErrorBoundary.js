346e2f4376e2b9a385b993268733b0ff845f2fb1
Jennifer Tchai <jentchai@gmail.com>
2022-06-13 21:59:45 -0400

    updated error messaging

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -1,8 +1,8 @@
 import { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 
 class ErrorBoundary extends Component {
-  state = { hasError: false };
+  state = { hasError: false, redirect: false };
   static getDerivedStateFromError() {
     return { hasError: true }
   }
@@ -11,12 +11,20 @@ class ErrorBoundary extends Component {
     console.error(error, info);
   }
 
-  render () {
+  componentDidUpdate() {
     if (this.state.hasError) {
+      setTimeout(() => this.setState({ redirect: true }), 5000);
+    }
+  }
+
+  render () {
+    if (this.state.redirect) {
+      return <Navigate to='/' />
+    } else if (this.state.hasError) {
       return (
         <h2>
           There was an error. Oops.
-          <Link>Click here</Link> to go back to the homepage.
+          <Link to="/">Click here</Link> to go back to the homepage. Or wait 5 seconds to be automatically redirected.
         </h2>
       );
     }
