94f7d35f7059cf2dd01ac435591a36dbedaf2612
Jennifer Tchai <jentchai@gmail.com>
2022-06-13 22:19:44 -0400

    got rid of error

--- src/Details.js
+++ src/Details.js
@@ -19,7 +19,7 @@ class Details extends Component {
       return <h2>loading … </h2>;
     }
 
-    throw new Error("lmao you crashed");
+    // throw new Error("lmao you crashed");
 
     const { animal, breed, city, state, description, name, images } =
       this.state;
