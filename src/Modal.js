8c1b3c4ba9185c5e0f3ebc31b8f2d4bda9b53dcd
Jennifer Tchai <jentchai@gmail.com>
2022-06-20 19:40:53 -0400

    experimenting with useRef

--- src/Modal.js
+++ src/Modal.js
@@ -1,6 +1,11 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
+// useRef: across all the renderings I want to reference the same thing; I want the exact same thing in every render
+
 const Modal ({ children }) => {
   const elementRef = useRef(null);
+  if (!elementRef.current) {
+    elementRef.current = document.createElement('div');
+  }
 }
