5307b7ec21d2453b19bbe67263019479e862c9c9
Jennifer Tchai <jentchai@gmail.com>
2022-06-20 19:58:25 -0400

    removed useRef comment

--- src/Modal.js
+++ src/Modal.js
@@ -1,8 +1,6 @@
 import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 
-// useRef: across all the renderings I want to reference the same thing; I want the exact same thing in every render
-
 const Modal ({ children }) => {
   const elementRef = useRef(null);
   if (!elementRef.current) {
